350c712681f0a91ffed5471ecd3a0a51254d397b
mutasem <motasemimad10@gmail.com>
2021-10-11 16:31:25 +0300

    Building the Main Section

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,14 @@ export default function Home() {
 
       <Header />
       <Banner />
+
+      <main className='max-w-7xl mx-auto px-8 sm:px-16'>
+        <section className='pt-6'>
+          <h2 className='text-4xl font-semibold pb-5'>Explore Nearby</h2>
+
+          {/* Pull some data from a severe / API endpoints */}
+        </section>
+      </main>
     </div>
   );
 }
