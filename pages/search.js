f23fab4e9f1427b8cb71ca501e3c925f481f9cc4
mutasem <motasemimad10@gmail.com>
2021-10-12 18:24:12 +0300

    Adding onto the Search Component

--- pages/search.js
+++ pages/search.js
@@ -52,7 +52,7 @@ const Search = ({ searchResult }) => {
           </div>
         </section>
 
-        <section className='hidden xl:inline-flex'>
+        <section className='hidden xl:inline-flex xl:min-w-[600px]'>
           <Map />
         </section>
       </main>
